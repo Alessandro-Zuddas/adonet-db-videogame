5dd2ce655b09971130c5bdfbfa6a44e015d6aa59
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-03-30 16:13:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data.SqlClient;
+using SqlClient;
+
+var connStr = "Data Source=localhost;Initial Catalog=db_videogames;Integrated Security=True";
+var videogameManager = new VideogameManager(connStr);
+
+Console.WriteLine("Ricerca gioco per ID");
+var userId = Convert.ToInt64(Console.ReadLine());
+
+
+var videogame = videogameManager.GetVideogameById(userId);
+
+foreach (var item in videogame) Console.WriteLine(item);
+
+
+Console.WriteLine("Ricerca per nome");
+var userName = Console.ReadLine();
+
+
+var videogameName = videogameManager.GetVideoGameByNameLike(userName);
+
+
+foreach (var item in videogameName) Console.WriteLine(item);
+
+Console.WriteLine($"Count: {videogameName.Count}");
